6a922d9dbde43ac4d6e792066b9f3f00e3a4f22d
Maksym Nedvyha <imus663@gmail.com>
2020-05-09 15:44:41 +0300

    feat(recite.component): add recipe component

---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Meal } from '../meal';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.scss']
+})
+export class RecipeComponent implements OnInit {
+
+  recipe: Meal = new Meal();
+  id: string;
+
+  private querySubscription: Subscription;
+
+  constructor(private route: ActivatedRoute, private http: HttpService) {
+    this.querySubscription = route.queryParams.subscribe((queryParam: any) => {
+      // tslint:disable-next-line: no-string-literal
+      this.id = queryParam['id'];
+    });
+  }
+
+  ngOnInit(): void {
+    this.http.getMealById(this.id).subscribe(data => this.recipe = data[0]);
+  }
+
+  private filterItem(item: string): boolean {
+    if (item !== null && item !== '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  filterItems(item1: string, item2: string): boolean {
+    if (this.filterItem(item1) && this.filterItem(item2)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
