a9136797edec79cc375e24cd4fc2b2ad59f39bb6
Maksym Nedvyha <imus663@gmail.com>
2020-05-09 15:55:40 +0300

    feat(app-routing.module): add routing for website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recipe', component: RecipeComponent },
+  { path: 'recipe-list', component: RecipeListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
