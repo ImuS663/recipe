f19db8f2975c2466d602205740dd925cb426d36c
Maksym Nedvyha <imus663@gmail.com>
2020-05-10 01:15:36 +0300

    fix(recile-list.component): fixed recipe loading by category and area

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -15,7 +15,9 @@ export class RecipeListComponent implements OnInit, DoCheck {
   private searchName: string;
   private searchNameBack: string;
   private category: string;
+  private categoryBack: string;
   private area: string;
+  private areaBack: string;
 
   private querySubscription: Subscription;
 
@@ -24,25 +26,22 @@ export class RecipeListComponent implements OnInit, DoCheck {
       // tslint:disable-next-line: no-string-literal
       this.searchNameBack = queryParam['search'];
       // tslint:disable-next-line: no-string-literal
-      this.category = queryParam['c'];
+      this.categoryBack = queryParam['c'];
       // tslint:disable-next-line: no-string-literal
-      this.area = queryParam['a'];
+      this.areaBack = queryParam['a'];
     });
-    if (this.searchNameBack !== null) {
-      this.search();
-    } else if (this.category !== null) {
-      this.http.filteringByFilterCategory(this.category).subscribe(data => this.recipeList = data);
-    } else if (this.area !== null) {
-      this.http.filteringByFilterArea(this.area).subscribe(data => this.recipeList = data);
-    }
   }
 
   ngOnInit(): void {
   }
 
   ngDoCheck(): void {
-    if (this.searchNameBack !== null) {
+    if (this.searchNameBack !== undefined) {
       this.search();
+    } else if (this.categoryBack !== undefined) {
+      this.filterCategory();
+    } else if (this.areaBack !== undefined) {
+      this.filterArea();
     }
   }
 
@@ -50,6 +49,19 @@ export class RecipeListComponent implements OnInit, DoCheck {
     if (this.searchNameBack !== this.searchName) {
       this.searchName = this.searchNameBack;
       this.http.search(this.searchName).subscribe(data => this.recipeList = data);
+    }
+  }
+
+  private filterCategory(): void {
+    if (this.categoryBack !== this.category) {
+      this.category = this.categoryBack;
+      this.http.filteringByFilterCategory(this.category).subscribe(data => this.recipeList = data);
+    }
+  }
+  private filterArea(): void {
+    if (this.areaBack !== this.area) {
+      this.area = this.areaBack;
+      this.http.filteringByFilterArea(this.area).subscribe(data => this.recipeList = data);
     }
   }
 }
